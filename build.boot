38f08287a78aac3b751dd6cc05a1101f9a9c6e4e
Daehyun Kim <hatemogi@gmail.com>
2016-08-30 11:21:44 +0900

    레이아웃 생성

--- build.boot
+++ build.boot
@@ -2,6 +2,7 @@
  :source-paths #{"src"}
  :resource-paths #{"res"}
  :dependencies '[[org.clojure/clojure "1.8.0"]
-                 [hiccup "1.0.5"]])
+                 [hiccup "1.0.5"]
+                 [markdown-clj "0.9.89"]])
 
 (require '[ohucode.www.boot :refer :all])
--- src/ohucode/www/boot.clj
+++ src/ohucode/www/boot.clj
@@ -4,12 +4,13 @@
             [clojure.string :as s]
             [boot.core :as b]
             [boot.task.built-in :as task]
+            [boot.util :refer [info]]
             [hiccup.core :refer [h]]
             [hiccup.page :refer [html5 include-css include-js]]))
 
 (defn layout
   "HTML 기본 레이아웃"
-  [content]
+  [& contents]
   (html5 [:head
           [:meta {:charset "utf-8"}]
           [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
@@ -18,34 +19,35 @@
                ["https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
                 "https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
                 "css/ohucode.css"])]
-         [:body
-          [:main
-           content
-           (map include-js ["https://code.jquery.com/jquery-3.1.0.min.js"
-                            "https://d3js.org/d3.v4.min.js"
-                            "js/ohucode.js"])]]))
+         (-> [:body]
+             (into contents)
+             (into (map include-js ["https://code.jquery.com/jquery-3.1.0.min.js"
+                                    "https://d3js.org/d3.v4.min.js"
+                                    "js/ohucode.js"])))))
+
+(defn page
+  "네비게이션 포함 레이아웃"
+  [& contents]
+  (layout [:nav]
+          (into [:main] contents)
+          [:footer "푸터"]))
 
 (defn convert [f in out]
   (io/make-parents out)
   (spit out (f (slurp in))))
 
 (b/deftask markdown
-  "레이아웃 생성"
+  "마크다운 페이지 변환"
   []
   (let [tmp (b/tmp-dir!)]
     (b/with-pre-wrap fileset
-      (let [in  (b/input-files fileset)
-            mds (b/by-ext [".md" ".markdown"] in)]
-        (doseq [md mds]
-          (let [infile  (b/tmp-file md)
-                inpath  (b/tmp-path md)
-                outpath (s/replace inpath #"\.(md|markdown)$" ".html")
-                outfile (io/file tmp outpath)]
-            (println "--- " inpath  " => " outpath " ---")
-            (convert identity infile outfile))))
-      (-> fileset
-          (b/add-resource tmp)
-          b/commit!))))
+      (doseq [md (b/by-ext [".md" ".markdown"] (b/input-files fileset))]
+        (let [in-path  (b/tmp-path md)
+              out-path (s/replace in-path #"\.(md|markdown)$" ".html")
+              out-file (io/file tmp out-path)]
+          (info in-path  " => " out-path)
+          (convert layout (b/tmp-file md) out-file)))
+      (b/commit! (b/add-resource fileset tmp)))))
 
 (b/deftask build
   "빌드 태스크"
