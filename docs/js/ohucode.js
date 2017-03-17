4b7ca77c349efe38acdebb904470b8079e9e0d42
Daehyun Kim <hatemogi@gmail.com>
2017-03-17 22:18:57 +0900

    레이아웃 업데이트

--- docs/js/ohucode.js
+++ docs/js/ohucode.js
@@ -1,3 +1,9 @@
 (function ($) {
-    hljs.initHighlightingOnLoad();
-})(jQuery);
+    hljs.initHighlightingOnLoad()
+    $(function() {
+      const href = document.location.href
+      $('ul.nav a').filter(function(idx, a) {
+        return href.endsWith(a.attributes["href"].value)
+      }).parent().addClass("active")
+    })
+})(jQuery)
--- src/ohucode/www/boot.clj
+++ src/ohucode/www/boot.clj
@@ -20,33 +20,34 @@
                ["https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
                 "https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
                 "http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.6.0/styles/default.min.css"
-                "css/cover.css"
+                "css/jumbotron.css"
                 "css/ohucode.css"])]
-         (-> [:body]
-             (into contents)
-             (into (map include-js ["https://code.jquery.com/jquery-3.1.0.min.js"
-                                    "https://d3js.org/d3.v4.min.js"
-                                    "http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.6.0/highlight.min.js"
-                                    "js/ohucode.js"])))))
+         [:body contents
+          (map include-js ["https://code.jquery.com/jquery-3.1.0.min.js"
+                           "https://d3js.org/d3.v4.min.js"
+                           "http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.6.0/highlight.min.js"
+                           "js/ohucode.js"])]))
 
 (defn cover
   "커버 페이지 레이아웃"
   [& contents]
-  (layout [:div.site-wrapper
-           [:div.site-wrapper-inner
-            [:div.cover-container
-             [:div.masthead.clearfix
-              [:div.inner
-               [:h3.masthead-brand "오후코드"]
-               [:nav.masthead-nav
-                [:ul.nav.masthead-nav
-                 [:li [:a {:href "index.html"} "소개"]]
-                 [:li [:a {:href "projects.html"} "프로젝트"]]
-                 [:li [:a {:href "contact.html"} "연락처"]]]]]]
-             [:div.inner.cover (into [:main] contents)]
-             [:div.mastfoot
-              [:div.inner
-               [:p [:a {:href "https://medium.com/@hatemogi"} "@hatemogi"]]]]]]]))
+  (layout [:div.container
+           [:div.header.clearfix
+            [:nav
+             [:ul.nav.nav-pills.pull-right
+              [:li {:role "presentation"} [:a {:href "index.html"} "소개"]]
+              [:li {:role "presentation"} [:a {:href "projects.html"} "프로젝트"]]
+              [:li {:role "presentation"} [:a {:href "contact.html"} "연락처"]]]]
+            [:h3.text-muted "오후코드"]]
+           [:div.jumbotron
+            [:h2 "프리랜서 서버 개발자"]
+            [:p "국내 최대 수준의 웹서비스 트래픽을 처리한 경험을 바탕으로, 탄탄하고 빠른 서버 소프트웨어를 개발해드립니다"]]
+           [:div.row.marketing contents]
+           [:footer.footer
+            [:p.pull-right [:a {:href "https://medium.com/@hatemogi"}
+                            [:i.fa.fa-medium] " hatemogi"]]
+            [:p [:a {:href "https://kr.linkedin.com/in/hatemogi"}
+                            [:i.fa.fa-linkedin] " 링크드인"]]]]))
 
 (defn md->html "마크다운을 HTML로 변환"
   [text]
